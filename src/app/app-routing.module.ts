c0e96b60c333c7c12a1664bc12d308fbbfbe45c7
Rafaaquino <rafaaquino28@gmail.com>
2021-08-03 18:03:25 -0300

    routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PageComponent } from './pages/home/page.component';
 
+import { PageComponent } from './pages/home/page.component';
 import { TermsComponent } from './pages/terms/terms.component';
 import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
 
@@ -10,7 +10,7 @@ const routes: Routes = [
 
   { path: '', component: PageComponent },
   { path: 'politicas-de-privacidade', component: PrivacyPolicyComponent },
-  { path: 'termos-de-uso', component: TermsComponent },
+  { path: 'termos-de-uso', component: TermsComponent }
 
 ];
 
