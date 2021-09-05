2473d0bf4c45d0ec645e94d9dafc9cda6f17301a
Paulo <pauloh2012sul@gmail.compwd>
2021-09-04 23:55:25 -0300

    ajustes

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -12,6 +12,7 @@ import { AuthGuard } from './_services/auth.guard';
 import { LoggedPagesComponent } from './pages/admin/logged-pages/logged-pages.component';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+import { RegisterComponent } from './pages/account/register/register.component';
 
 const adminModule = () => import('./pages/admin/admin-layout.module').then(x => x.AdminLayoutModule);
 
@@ -24,6 +25,7 @@ const routes: Routes = [
   { path: 'faq', component: FAQComponent },
   { path: 'politicas-de-privacidade', component: PrivacyPolicyComponent },
   { path: 'termos-de-uso', component: TermsComponent },
+  { path: 'register', component: RegisterComponent },
   {
     path: '',
     component: LoggedPagesComponent,
